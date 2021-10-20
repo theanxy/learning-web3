b5f18d72e5576722cbbd51784bc7197d318cfd66
Wojtek Zając <wojtek@wojtekzajac.com>
2021-10-20 19:13:01 +0100

    Section 2, step 2

--- buildspace/dapp-course/frontend/pages/index.js
+++ buildspace/dapp-course/frontend/pages/index.js
@@ -1,9 +1,33 @@
-import styles from '../styles.module.css'
+import { useEffect } from 'react';
+import { ethers } from 'ethers';
+
+import styles from '../styles.module.css';
 
 export default function Home() {
+  const checkIfWalletIsConnected = () => {
+    /*
+     * First make sure we have access to window.ethereum
+     */
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      console.log('Make sure you have metamask!');
+      return;
+    } else {
+      console.log('We have the ethereum object', ethereum);
+    }
+  };
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
   return (
     <div className={styles.hello}>
       <p>My first DApp!</p>
+      <button className="waveButton" onClick={null}>
+        Wave at Me
+      </button>
     </div>
-  )
+  );
 }
